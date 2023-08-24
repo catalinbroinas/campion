7c88a9e519bd699d7589890abe1d4ab50c823e7c
catalinbroinas <catalinbroinas@yahoo.com>
2023-08-24 18:04:30 +0300

    Cover, JS date functions and navbar design

---
+++ src/js/style.js
@@ -0,0 +1,89 @@
+
+function thisDate()
+{
+  const current_date = new Date();
+  return current_date;
+}
+
+function thisYear()
+{
+  const current_date = thisDate();
+  const current_year = current_date.getFullYear();
+  return current_year;
+}
+
+function thisMonth()
+{
+  const current_date = thisDate();
+  const month = current_date.getMonth();
+
+  let current_month;
+
+  switch(month)
+  {
+    case 0:
+      current_month = "Ianuarie";
+      break;
+    case 1:
+      current_month = "Februarie";
+      break;
+    case 2:
+      current_month = "Martie";
+      break;
+    case 3:
+      current_month = "Aprilie";
+      break;
+    case 4:
+      current_month = "Mai";
+      break;
+    case 5:
+      current_month = "Iunie";
+      break;
+    case 6:
+      current_month = "Iulie";
+      break;
+    case 7:
+      current_month = "August";
+      break;
+    case 8:
+      current_month = "Septembrie";
+      break;
+    case 9:
+      current_month = "Octombrie";
+      break;
+    case 10:
+      current_month = "Noiembrie";
+      break;
+    case 11:
+      current_month = "Decembrie";
+  }
+
+  return current_month;
+}
+
+function navbarScroll()
+{
+  const navbar = document.getElementById("main-navbar");
+
+  window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 0) {
+        navbar.classList.add("navbar-after-scroll")
+      } else {
+        navbar.classList.remove("navbar-after-scroll")
+      }
+    })
+}
+
+function myDate()
+{
+  const current_date = thisDate();
+  const current_month = thisMonth(); 
+  const current_year = thisYear();
+
+  document.getElementById("intro-title").innerHTML = "Azi, "
+   + current_date.getDate() + " " + current_month
+   + " " + current_year;
+}
+
+navbarScroll();
+myDate();
